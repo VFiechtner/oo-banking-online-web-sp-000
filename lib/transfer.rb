a9274f3fb541cb5845fd73fd14133865ebe99043
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-07-11 18:34:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,14 +15,10 @@ class Transfer
     @sender.valid? && @receiver.valid? ? true : false
   end
 
-  def closed_account_transfer
+  def execute_transaction
     if @status == "closed"
       return "Transaction rejected. Please check your account balance."
-    end
-  end
-
-  def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
+    elsif @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
