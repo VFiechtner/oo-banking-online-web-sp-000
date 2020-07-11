5d48dfbd96239d589b4e1f693520bc0657070c71
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2020-07-11 18:23:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,16 +16,13 @@ class Transfer
   end
 
   def execute_transaction
-    if
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-    else
-      @sender.balance > @amount && @status == "pending"
+    if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-      #binding.pry
-
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
     end
   end
 
